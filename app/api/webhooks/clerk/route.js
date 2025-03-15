c839294f6fba3943886020c4e65ea72f1101a1c8
Yash Vishnoi <88367411+YashVishnoi47@users.noreply.github.com>
2025-03-15 16:48:17 +0530

    fix: correct email address property name in Clerk webhook integration

--- app/api/webhooks/clerk/route.js
+++ app/api/webhooks/clerk/route.js
@@ -58,7 +58,7 @@ export async function POST(req) {
 
     const user = {
       clerkId: id,
-      email: email_addresses[0].email_Address,
+      email: email_addresses[0].email_address,
       photo: image_url,
       firstName: first_name,
       lastName: last_name,
