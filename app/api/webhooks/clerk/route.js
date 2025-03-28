9165ee84b89ba71941e204adfe84e4abd48fb421
Yash Vishnoi <88367411+YashVishnoi47@users.noreply.github.com>
2025-03-28 16:35:26 +0530

    feat: import NextResponse for improved response handling in webhook route

--- app/api/webhooks/clerk/route.js
+++ app/api/webhooks/clerk/route.js
@@ -2,6 +2,7 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
+import { NextResponse } from "next/server";
 
 export async function POST(req) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET;
